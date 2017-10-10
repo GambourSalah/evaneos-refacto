8ef5259be09bbc7fa229dc635ce96b6fb2c3d12f
Slimdwet <rinville.evans@gmail.com>
2017-10-10 21:19:41 +0200

    Import the entity's factory

--- example/example.php
+++ example/example.php
@@ -9,6 +9,7 @@ require_once __DIR__ . '/../src/Entity/Template.php';
 require_once __DIR__ . '/../src/Entity/User.php';
 require_once __DIR__ . '/../src/Helper/SingletonTrait.php';
 require_once __DIR__ . '/../src/Context/ApplicationContext.php';
+require_once __DIR__ . '/../src/Repository/FactoryRepository.php';
 require_once __DIR__ . '/../src/Repository/Repository.php';
 require_once __DIR__ . '/../src/Repository/DestinationRepository.php';
 require_once __DIR__ . '/../src/Repository/QuoteRepository.php';
